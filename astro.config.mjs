d6a318e95a9b9c230d70a2630f623b92ffd1fc54
Steve <stevedsimkins@gmail.com>
2023-06-13 19:28:56 -0400

    moved around gateways section

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,28 +13,28 @@ export default defineConfig({
         {
           label: 'Welcome to Pinata!',
           items: [
-            { label: 'Introduction', link: '/'}
+            { label: 'Introduction', link: '/' }
           ]
         },
         {
           label: 'Start Here',
           items: [
-            { label: "What Can I Learn Here?", link: '/start-here/'},
-            { label: "Getting Started", link: '/start-here/getting-started/'},
-            { label: 'Features', items: [
-              { label: "Gateways", items: [
-                { label: "Gateways", link: '/start-here/features/gateways/gateways' },
-                { label: "The Public Pinata Gateway", link: '/start-here/features/gateways/the-public-pinata-gateway' },
-                { label: "Dedicated Gateways", link: '/start-here/features/gateways/dedicated-gateways' },
-                { label: "How do I use My Gateway?", link: '/start-here/features/gateways/how-do-i-use-my-gateway' },
-                { label: "Open vs Restricted", link: '/start-here/features/gateways/open-vs.-restricted' },
-              ]},
-              { label: 'Image Optimizations', link: '/start-here/features/image-optimization'},
-              { label: 'Rate Limits', link: '/start-here/features/rate-limits'},
-              { label: 'Video Streaming', link: '/start-here/features/video-streaming'},
-            ]}
+            { label: "What Can I Learn Here?", link: '/start-here/' },
+            { label: "Getting Started", link: '/start-here/getting-started/' },
           ]
         },
+        {
+          label: 'Gateways', items: [
+            { label: "Gateways", link: '/gateways/gateways' },
+            { label: "The Public Pinata Gateway", link: '/gateways/the-public-pinata-gateway' },
+            { label: "Dedicated Gateways", link: '/gateways/dedicated-gateways' },
+            { label: "How do I use My Gateway?", link: '/gateways/how-do-i-use-my-gateway' },
+            { label: "Open vs Restricted", link: '/gateways/open-vs-restricted' },
+            { label: 'Image Optimizations', link: '/gateways/image-optimization' },
+            { label: 'Rate Limits', link: '/gateways/rate-limits' },
+            { label: 'Video Streaming', link: '/gateways/video-streaming' },
+          ]
+        }
       ],
     }),
   ],
