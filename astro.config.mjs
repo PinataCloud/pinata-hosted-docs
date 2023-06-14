197eabb8d2878f860ac69440dd6203b176f1e07e
Steve <stevedsimkins@gmail.com>
2023-06-14 11:44:39 -0400

    added some more front page changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
+      title: 'Pinata Docs',
       social: {
         github: 'https://github.com/withastro/starlight',
       },
